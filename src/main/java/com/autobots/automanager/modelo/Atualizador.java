eb7949f970c97b22e8964f8f4f71e75a904f9534
BrunoSerpa <serpabruno2005@gmail.com>
2024-08-07 09:12:08 -0300

    Arrumei o atualizar cliente

--- src/main/java/com/autobots/automanager/modelo/Atualizador.java
+++ src/main/java/com/autobots/automanager/modelo/Atualizador.java
@@ -5,86 +5,91 @@ import java.util.List;
 import com.autobots.automanager.entidades.*;
 
 public class Atualizador {
-	private StringVerificadorNulo verificador = new StringVerificadorNulo();
+    private StringVerificadorNulo verificador = new StringVerificadorNulo();
 
-	public void atualizarCliente(Cliente cliente, Cliente atualizacao){
-		if (!verificador.verificar(atualizacao.getNome())) {
-			cliente.setNome(atualizacao.getNome());
-		}
-		if (!verificador.verificar(atualizacao.getNomeSocial())) {
-			cliente.setNomeSocial(atualizacao.getNomeSocial());
-		}
-		if (!(atualizacao.getDataCadastro() == null)) {
-			cliente.setDataCadastro(atualizacao.getDataCadastro());
-		}
-		if (!(atualizacao.getDataNascimento() == null)) {
-			cliente.setDataNascimento(atualizacao.getDataNascimento());
-		}
-		atualizarEndereco(cliente.getEndereco(), atualizacao.getEndereco());
-		atualizarDocumentos(cliente.getDocumentos(), atualizacao.getDocumentos());
-		atualizarTelefones(cliente.getTelefones(), atualizacao.getTelefones());
-	}
+    public void atualizarCliente(Cliente cliente, Cliente atualizacao){
+        if (!verificador.verificar(atualizacao.getNome())) {
+            cliente.setNome(atualizacao.getNome());
+        }
+        if (!verificador.verificar(atualizacao.getNomeSocial())) {
+            cliente.setNomeSocial(atualizacao.getNomeSocial());
+        }
+        if (atualizacao.getDataCadastro() != null) {
+            cliente.setDataCadastro(atualizacao.getDataCadastro());
+        }
+        if (atualizacao.getDataNascimento() != null) {
+            cliente.setDataNascimento(atualizacao.getDataNascimento());
+        }
 
-	public void atualizarEndereco(Endereco endereco, Endereco atualizacao){
-		if (!verificador.verificar(atualizacao.getEstado())) {
-			endereco.setEstado(atualizacao.getEstado());
-		}
-		if (!verificador.verificar(atualizacao.getCidade())) {
-			endereco.setCidade(atualizacao.getCidade());
-		}
-		if (!verificador.verificar(atualizacao.getBairro())) {
-			endereco.setBairro(atualizacao.getBairro());
-		}
-		if (!verificador.verificar(atualizacao.getRua())) {
-			endereco.setRua(atualizacao.getRua());
-		}
-		if (!verificador.verificar(atualizacao.getNumero())) {
-			endereco.setNumero(atualizacao.getNumero());
-		}
-		if (!verificador.verificar(atualizacao.getInformacoesAdicionais())) {
-			endereco.setInformacoesAdicionais(atualizacao.getInformacoesAdicionais());
-		}
-	}
+        if (atualizacao.getEndereco() != null) {
+            if (cliente.getEndereco() == null) {
+                cliente.setEndereco(atualizacao.getEndereco());
+            } else {
+                atualizarEndereco(cliente.getEndereco(), atualizacao.getEndereco());
+            }
+        }
+        if (atualizacao.getTelefones() != null) {
+            if (cliente.getTelefones() == null) {
+            	cliente.setTelefones(atualizacao.getTelefones());
+            } else {            	
+            	atualizarTelefones(cliente.getTelefones(), atualizacao.getTelefones());
+            }
+        }
+        if (atualizacao.getDocumentos() != null) {
+        	if (cliente.getDocumentos() == null) {
+        		cliente.setDocumentos(atualizacao.getDocumentos());
+        	} else {
+        		atualizarDocumentos(cliente.getDocumentos(), atualizacao.getDocumentos());
+        	}
+        }
+    }
 
-	public void atualizarDocumentos(List<Documento> documentos, List<Documento> atualizacoes){
-		for (Documento atualizacao : atualizacoes) {
-			for (Documento documento : documentos) {
-				if (atualizacao.getId() != null) {
-					if (atualizacao.getId() == documento.getId()) {
-						atualizarDocumento(documento, atualizacao);
-					}
-				}
-			}
-		}
-	}
+    public void atualizarEndereco(Endereco endereco, Endereco atualizacao){
+        if (!verificador.verificar(atualizacao.getEstado())) {
+            endereco.setEstado(atualizacao.getEstado());
+        }
+        if (!verificador.verificar(atualizacao.getCidade())) {
+            endereco.setCidade(atualizacao.getCidade());
+        }
+        if (!verificador.verificar(atualizacao.getBairro())) {
+            endereco.setBairro(atualizacao.getBairro());
+        }
+        if (!verificador.verificar(atualizacao.getRua())) {
+            endereco.setRua(atualizacao.getRua());
+        }
+        if (!verificador.verificar(atualizacao.getNumero())) {
+            endereco.setNumero(atualizacao.getNumero());
+        }
+        if (!verificador.verificar(atualizacao.getInformacoesAdicionais())) {
+            endereco.setInformacoesAdicionais(atualizacao.getInformacoesAdicionais());
+        }
+    }
 
-	public void atualizarDocumento(Documento documento, Documento atualizacao){
-		if (!verificador.verificar(atualizacao.getTipo())) {
-			documento.setTipo(atualizacao.getTipo());
-		}
-		if (!verificador.verificar(atualizacao.getNumero())) {
-			documento.setNumero(atualizacao.getNumero());
-		}
-	}
+    public void atualizarDocumentos(List<Documento> documentos, List<Documento> atualizacoes){
+        for (Documento atualizacao : atualizacoes) {
+            for (Documento documento : documentos) {
+                if (atualizacao.getId() != null) {
+                    if (atualizacao.getId().equals(documento.getId())) {
+                        atualizarDocumento(documento, atualizacao);
+                    }
+                }
+            }
+        }
+    }
 
-	public void atualizarTelefones(List<Telefone> telefones, List<Telefone> atualizacoes){
-		for (Telefone atualizacao : atualizacoes) {
-			for (Telefone telefone : telefones) {
-				if (atualizacao.getId() != null) {
-					if (atualizacao.getId() == telefone.getId()) {
-						atualizarTelefone(telefone, atualizacao);
-					}
-				}
-			}
-		}
-	}
+    public void atualizarDocumento(Documento documento, Documento atualizacao){
+        if (!verificador.verificar(atualizacao.getTipo())) {
+            documento.setTipo(atualizacao.getTipo());
+        }
+        if (!verificador.verificar(atualizacao.getNumero())) {
+            documento.setNumero(atualizacao.getNumero());
+        }
+    }
 
-	public void atualizarTelefone(Telefone telefone, Telefone atualizacao){
-		if (!verificador.verificar(atualizacao.getDdd())) {
-			telefone.setDdd(atualizacao.getDdd());
-		}
-		if (!verificador.verificar(atualizacao.getNumero())) {
-			telefone.setNumero(atualizacao.getNumero());
-		}
-	}
+    public void atualizarTelefones(List<Telefone> telefones, List<Telefone> atualizacoes){
+        if (atualizacoes != null && !atualizacoes.isEmpty()) {
+            telefones.clear();
+            telefones.addAll(atualizacoes);
+        }
+    }
 }
