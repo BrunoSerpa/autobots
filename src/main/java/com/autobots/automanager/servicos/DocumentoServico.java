76a2a53ba4771157c2da1fa96cdc0b7a9c75c819
BrunoSerpa <serpabruno2005@gmail.com>
2024-08-07 21:06:19 -0300

    removi imports não utilizados

--- src/main/java/com/autobots/automanager/servicos/DocumentoServico.java
+++ src/main/java/com/autobots/automanager/servicos/DocumentoServico.java
@@ -4,12 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.autobots.automanager.entidades.Documento;
-import com.autobots.automanager.modelo.Atualizador;
-import com.autobots.automanager.modelo.DocumentoValidador;
 import com.autobots.automanager.repositorios.DocumentoRepositorio;
 
 @Service
